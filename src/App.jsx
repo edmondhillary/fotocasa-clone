e01769d4d050463430954a5fe04170b20f8362da
Eduardo Gonzalez <edu14937@gmail.com>
2023-06-30 15:59:49 +0200

    first commit of my fotocasa-clone front- page

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import React, {useEffect, useState} from 'react';
+import './App.css'
+
+
+
+
+function App() {
+    return (
+      <div className="App">
+        Modelo de Vite,  App, ESTAS SEGURO NO  ANDAS TOCANDO EL MODELO, PQ TE MATO 
+      </div>
+    );
+  }
+  
+  export default App;
+  
